24ba24e3b483e3a75676aafbc92b700c8860e195
natasha170490 <42786373+natasha170490@users.noreply.github.com>
2018-09-07 12:00:33 -0400

    final edits
    I am trying to modify the title of the x axis to be lower.

---
+++ HW1-natashag-vf.R
@@ -0,0 +1,59 @@
+#I am going to create the UI of a NavBar for the Esoph package
+
+library(shiny)
+library(reshape2)
+library(dplyr)
+library(plotly)
+library(shinythemes)
+library(DT)
+
+cancer<-esoph
+
+ui <- fluidPage(
+  navbarPage("Smoking, Alcohol and Esophageal Cancer NavBar", 
+             theme = shinytheme("united"),
+             tabPanel("Plot",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("agegp_select",
+                                      "Age:",
+                                      choices = levels(cancer$agegp),
+                                      multiple = TRUE,
+                                      selectize = TRUE,
+                                      selected = c("25-34", "35-44", "45-54", "55-64","65-74","75+")),
+                          sliderInput("ncontrolsSelect",
+                                      "Number of Controls:",
+                                      min = min(cancer$ncontrols, na.rm = T),
+                                      max = max(cancer$ncontrols, na.rm = T),
+                                      value = c(min(cancer$ncontrols, na.rm = T), max(cancer$ncontrols, na.rm = T)),
+                                      step = 1)
+                        ),
+                        # Output plot
+                        mainPanel(
+                          plotlyOutput("plot")
+                        )
+                      )
+             ),
+             # Data Table
+             tabPanel("Table",
+                      fluidPage(DT::dataTableOutput("table"))
+             )
+  )
+)
+
+# I will define the server 
+server <- function(input, output) {
+  output$plot <- renderPlotly({
+    ggplot(data = cancer, aes(x = alcgp, y= ncases, fill = tobgp)) + geom_bar(stat="identity")+ 
+      facet_grid(.~agegp) +
+      labs(x="Alcohol level", y="Number of Cases of Cancer", title= "Smoking, Alcohol and Esophageal Cancer by Age",  fill= "Smoking Level")+ 
+      theme(text= element_text(size=7.5),axis.text.x=element_text(angle=45,vjust=1,hjust=1), axis.title.x=element_text(line=2.2))
+  }) 
+  output$table <- DT::renderDataTable({
+    subset(cancer, agegp %in% input$agegp_select, select = c(agegp, alcgp, tobgp, ncases, ncontrols))
+  })
+}
+
+#  I will run the application 
+shinyApp(ui = ui, server = server)
+    
